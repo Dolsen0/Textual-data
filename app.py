3bb97dfb8e5749206ec417c35b8e4404779bf014
dolsen0 <derek.olsen@gmail.com>
2023-01-04 23:45:49 -0500

    added user input to retrieve tags, noun phrases, and sentiment data

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from textblob import TextBlob
 
-text = "Hello everyone. I'm here today to inform you that there is a new technology that we would like to implement which will allow us to process textual data. Let's see the results provided with the previous two sentences."
+# Docs for textblob can be found:
+# https://textblob.readthedocs.io/en/latest/quickstart.html#part-of-speech-tagging
+
+
+text = input("Enter a sentence to determine sentence: tags, noun phrases, polarity, and sentiment \n")
 
 blob = TextBlob(text)
 
-print(blob.noun_phrases)
+
+
+print(f" \n the following are tags: \n {blob.tags}")    # prints part of speech tags
+print(f" \n the following are noun phrases: \n{blob.noun_phrases}")    #noun phrases 
+print(f" \n the following shows the polarity and sentiment: \n {blob.sentiment}")   # "The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective."
+
